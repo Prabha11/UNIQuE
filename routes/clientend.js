c50a2f31ab04dbdd0ed07f507b9346d5ccd28eb9
Prabhath Bandara <okprabhath@gmail.com>
2018-10-14 02:07:42 +0530

    0208

--- routes/clientend.js
+++ routes/clientend.js
@@ -23,10 +23,50 @@ router.get('/', function(req, res, next) {
     //res.render('clientend', { title: 'Client'});
 })
 
-router.post('/subcribe',function (req, res) {
+router.post('/subscribe',function (req, res,next) {
     var userName = req.body.username;
     var organization = req.body.organization;
-    console.log("got a subscription request from "+ userName)
+    var type = req.body.type;
+    console.log("got a subscription request from " + userName)
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("unique");
+        var myobj = {
+            subscription_company:organization,
+            subscription_by:userName,
+            subscription_date:new Date(),
+            subscription_type:type
+        };
+        dbo.collection("subscription_table").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("unique");
+        var query = { user_email: userName };
+        dbo.collection("users_table").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+
+            var phone_no = result.user_phoneNo;
+
+            tapApi.sms.requestCreator({applicationId : "APP_000101", password : "password"}).single(phone_no, "You were subscribed to "+organization, function(mtReq){
+                tapApi.transport.createRequest({hostname: '127.0.0.1', port: 7000, path: '/sms/send'}, mtReq, function(request){
+                    tapApi.transport.httpClient(request, function() {
+                        console.log("Mt request send to subscriber" + mtReq)
+                    })
+                })
+            });
+        });
+    });
+
+});
 
 router.get('/tadhack', function(req, res, next) {
     MongoClient.connect(url, function(err, db) {
